0b3fe657ce7fc7222525667a00690cd286280372
Dhareppa12-art <dhareppabistagond17@gmail.com>
2025-08-24 13:52:43 +0530

    Added Middleware for create city API

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { CityController } = require('../../controllers');
+const { CityMiddlewares }  = require('../../middlewares')
 
 const router = express.Router();
 
 // remove the duplicate '/airplanes'
 router.post('/', 
+    CityMiddlewares.validateCreateRequest,
     CityController.createCity);
   
 
